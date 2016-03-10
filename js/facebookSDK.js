13851f41f5b3113430ce293b1058ee641d1ff5c7
Praveen Gupta <praveen@dhcp-ccc-8638.redrover.cornell.edu>
2016-03-10 17:50:28 -0500

    Adding login button

--- js/facebookSDK.js
+++ js/facebookSDK.js
@@ -20,23 +20,41 @@ function statusChangeCallback(response) {
     }
   }
 
-function checkLoginState() {
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
     });
   }
 
-window.fbAsyncInit = function() {
+  window.fbAsyncInit = function() {
   FB.init({
     appId      : '460148434191292',
-    xfbml      : true,
-    version    : 'v2.5'
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.5' // use graph api version 2.5
   });
-};
 
-FB.getLoginStatus(function(response) {
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
+  FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
   });
+
+  };
 
   // Load the SDK asynchronously
   (function(d, s, id) {
