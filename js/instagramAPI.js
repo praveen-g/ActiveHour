947b0d9dbd05e775c6a8f032ed4a31d562109910
Praveen Gupta <praveen@Praveens-MBP.home>
2016-03-11 16:56:01 -0500

    Extracting access token

--- js/instagramAPI.js
+++ js/instagramAPI.js
@@ -1,7 +1,9 @@
-var insatgram_access_token= "45159655.e98ee3c.84951d360aba46f2a57a365dacbcf88e"
-var getAccessToken= function(){
+var instagram_access_token= "";
+
+function getAccessToken(){
 	console.log(document.URL);
 	var exp="/token="
 	instagram_access_token = document.URL.match(exp)
 	console.log(instagram_access_token)
-}
+	instagram_access_token=instagram_access_token.substring(7)
+};
