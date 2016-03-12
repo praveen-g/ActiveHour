98c3438689e31f74b9a1f588e230ab8fe766522f
Praveen Gupta <praveen@Praveens-MBP.home>
2016-03-11 19:39:05 -0500

    Trying Twitter

--- js/instagramAPI.js
+++ js/instagramAPI.js
@@ -1,7 +1,7 @@
 var instagram_access_token= "";
 
 function getAccessToken(){
-	window.location.href="https://api.instagram.com/oauth/authorize/?client_id= e98ee3c6563a4c548e689fb10276e81c&redirect_uri=http://praveen-g.github.io/ActiveHour/&response_type=token";
+	window.location.href="https://api.instagram.com/oauth/authorize/?client_id=e98ee3c6563a4c548e689fb10276e81c&redirect_uri=http://praveen-g.github.io/ActiveHour/&response_type=token";
 	console.log(document.URL);
 	var exp="/token=";
 	instagram_access_token = document.URL.match(exp);
