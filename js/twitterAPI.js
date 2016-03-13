b300a172ff8fe48f2572cf52480815a7c459a2fe
Praveen Gupta <praveen@Praveens-MBP.home>
2016-03-13 04:18:49 -0400

    Twitter Authorizaton complete. Gaining Access Token4

--- js/twitterAPI.js
+++ js/twitterAPI.js
@@ -36,66 +36,42 @@ function getTwitterAccessToken(){
             // consider storing the token in a cookie or HTML5 local storage 
         }
     );
+}
 
-    var current_url = window.location.href.toString();
-    var query       = current_url.match(/\?(.+)$/).split("&amp;");
-    var parameters  = {};
-    var parameter;
+//     var current_url = window.location.href.toString();
+//     var query       = current_url.match(/\?(.+)$/).split("&amp;");
+//     var parameters  = {};
+//     var parameter;
 
     
    
-    for (var i = 0; i < query.length; i++) {
-        parameter = query[i].split("=");
-        if (parameter.length === 1) {
-            parameter[1] = "";
-        }
-        parameters[decodeURIComponent(parameter[0])] = decodeURIComponent(parameter[1]);
-    }
-
-    // check if oauth_verifier is set 
-    if (typeof parameters.oauth_verifier !== "undefined") {
-        console.log(parameters);
-        cb.setToken(stored_somewhere.oauth_token, stored_somewhere.oauth_token_secret);
-     
-        cb.__call(
-            "oauth_accessToken",
-            {
-                oauth_verifier: parameters.oauth_verifier
-            },
-            function (reply) {
-                cb.setToken(reply.oauth_token, reply.oauth_token_secret);
-     
-                // if you need to persist the login after page reload, 
-                // consider storing the token in a cookie or HTML5 local storage 
-            }
-        );
-    }
-}
-
-// };
-
-// cb.__call(
-//     "oauth_requestToken",
-//     {oauth_callback: "oob"},
-//     function (reply,rate,err) {
-//         if (err) {
-//             console.log("error response or timeout exceeded" + err.error);
+//     for (var i = 0; i < query.length; i++) {
+//         parameter = query[i].split("=");
+//         if (parameter.length === 1) {
+//             parameter[1] = "";
 //         }
-//         if (reply) {
-//             // stores it
-//             cb.setToken(reply.oauth_token, reply.oauth_token_secret);
-
-//             // gets the authorize screen URL
-//             cb.__call(
-//                 "oauth_authorize",
-//                 {},
-//                 function (auth_url) {
-//                     window.codebird_auth = window.open(auth_url);
-//                 }
-//             );
-//         }
+//         parameters[decodeURIComponent(parameter[0])] = decodeURIComponent(parameter[1]);
 //     }
-// );
+
+//     // check if oauth_verifier is set 
+//     if (typeof parameters.oauth_verifier !== "undefined") {
+//         console.log(parameters);
+//         cb.setToken(stored_somewhere.oauth_token, stored_somewhere.oauth_token_secret);
+     
+//         cb.__call(
+//             "oauth_accessToken",
+//             {
+//                 oauth_verifier: parameters.oauth_verifier
+//             },
+//             function (reply) {
+//                 cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+     
+//                 // if you need to persist the login after page reload, 
+//                 // consider storing the token in a cookie or HTML5 local storage 
+//             }
+//         );
+//     }
+// }
 
 // <script type="text/javascript">   
 // !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="http://platform.twitter.com/anywhere.js?id="+gTWApplicationId;fjs.parentNode.insertBefore(js,fjs);}}(document, "script",'twitter-anywhere');   
