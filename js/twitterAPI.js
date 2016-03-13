e50a36f2094792e98cb950e76be7579c031fceff
Praveen Gupta <praveen@Praveens-MBP.home>
2016-03-13 04:02:46 -0400

    Codebird oauth

--- js/twitterAPI.js
+++ js/twitterAPI.js
@@ -3,7 +3,36 @@ function twitterAccess(){
     
     var cb          = new Codebird;
     cb.setConsumerKey("5Y7kd4M5Ucwpufbj95dekJAa2", "6KukfHQyzcSx1AxX0jP9UqSPMKLeeKyBJSVURFbOINN6OyYQG4");
+
+    cb.__call(
+        "oauth_requestToken",
+        {oauth_callback: "oob"},
+        function (reply) {
+            // stores it 
+            cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+     
+            // gets the authorize screen URL 
+            cb.__call(
+                "oauth_authorize",
+                {},
+                function (auth_url) {
+                    window.codebird_auth = window.open(auth_url);
+                }
+            );
+        }
+    );
     
+    cb.__call(
+        "oauth_accessToken",
+        {oauth_verifier: document.getElementById("PINFIELD").value},
+        function (reply) {
+            // store the authenticated token, which may be different from the request token (!) 
+            cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+            console.log(reply)
+            // if you need to persist the login after page reload, 
+            // consider storing the token in a cookie or HTML5 local storage 
+        }
+    );
     var current_url = window.location.href.toString();
     var query       = current_url.match(/\?(.+)$/).split("&amp;");
     var parameters  = {};
