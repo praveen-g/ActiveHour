0fae30657c5f1c6dfd19440c513fe61d73cf9f33
Praveen Gupta <praveen@dhcp-ccc-11277.redrover.cornell.edu>
2016-03-14 01:43:35 -0400

    Hiding Instagram Button

--- js/instagramAPI.js
+++ js/instagramAPI.js
@@ -5,13 +5,18 @@ function changeURL(){
 	document.location="https://api.instagram.com/oauth/authorize/?client_id=e98ee3c6563a4c548e689fb10276e81c&redirect_uri=http://praveen-g.github.io/ActiveHour/&response_type=token";
 };
 
+function removeButton(){
+	var instagram_login_button = document.getElementById('instagramLoginButton');
+    instagram_login_button.style.display = "none";
+}
+
 function getAccessToken(){
 
 	console.log(document.location.hash);
 	
 	instagram_access_token = document.location.hash
 	console.log(instagram_access_token);
-	var instagram_login_button = document.getElementById('instagramLoginButton');
-    instagram_login_button.style.display = "none";
+	removeButton()
 }
 window.onload = getAccessToken();
+
