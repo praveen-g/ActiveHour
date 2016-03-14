26f65cf9c9b343290f7f63d6e4c5694eb232f05f
Rishav Kanoria <rishav.kanoria@gmail.com>
2016-03-13 21:59:55 -0400

    auth without PIN

--- js/twitterAPI.js
+++ js/twitterAPI.js
@@ -1,26 +1,68 @@
 var cb          = new Codebird;
 cb.setConsumerKey("5Y7kd4M5Ucwpufbj95dekJAa2", "6KukfHQyzcSx1AxX0jP9UqSPMKLeeKyBJSVURFbOINN6OyYQG4");
 
-function twitterAuthorize(){
-    
-    cb.__call(
-        "oauth_requestToken",
-        {oauth_callback: "oob"},
-        function (reply) {
-            // stores it 
-            cb.setToken(reply.oauth_token, reply.oauth_token_secret);
-     
-            // gets the authorize screen URL 
-            cb.__call(
-                "oauth_authorize",
-                {},
-                function (auth_url) {
-                    window.codebird_auth = window.open(auth_url);
-                }
-            );
-        }
+var twitterToken = "";
+var twitterSecret = "";
 
-    );
+function twitterAuthorize(){
+
+    var current_url = location.toString();
+    var query       = current_url.match(/\?(.+)$/).split("&amp;");
+    var parameters  = {};
+    var parameter;
+
+    for (var i = 0; i < query.length; i++) {
+        parameter = query[i].split("=");
+        if (parameter.length === 1) {
+            parameter[1] = "";
+        }
+        parameters[decodeURIComponent(parameter[0])] = decodeURIComponent(parameter[1]);
+    }
+
+    // check if oauth_verifier is set
+    if (typeof parameters.oauth_verifier !== "undefined") {
+        // assign stored request token parameters to codebird here
+        // ...
+        cb.setToken(stored_somewhere.oauth_token, stored_somewhere.oauth_token_secret);
+
+        cb.__call(
+            "oauth_accessToken",
+            {
+                oauth_verifier: parameters.oauth_verifier
+            },
+            function (reply, rate, err) {
+                if (err) {
+                    console.log("error response or timeout exceeded" + err.error);
+                }
+                if (reply) {
+                    cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+                }
+
+                // if you need to persist the login after page reload,
+                // consider storing the token in a cookie or HTML5 local storage
+            }
+        );
+    }
+    
+    // cb.__call(
+    //     "oauth_requestToken",
+    //     {oauth_callback: "oob"},
+    //     function (reply) {
+    //         // stores it 
+    //         cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+     
+    //         // gets the authorize screen URL 
+    //         cb.__call(
+    //             "oauth_authorize",
+    //             {},
+    //             function (auth_url) {
+    //                 window.codebird_auth = window.open(auth_url);
+    //             }
+    //         );
+    //     }
+
+    // );
+
 }
 
 function getTwitterAccessToken(){
@@ -32,6 +74,8 @@ function getTwitterAccessToken(){
             // store the authenticated token, which may be different from the request token (!) 
             cb.setToken(reply.oauth_token, reply.oauth_token_secret);
             console.log(reply)
+            twitterToken = reply.oauth_token;
+            twitterSecret = reply.oauth_token_secret;
             // if you need to persist the login after page reload, 
             // consider storing the token in a cookie or HTML5 local storage 
         }
