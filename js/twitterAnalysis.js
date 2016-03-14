95d17895239039651266ed2b06686b9200ab0ada
Praveen Gupta <praveen@dhcp-ccc-11277.redrover.cornell.edu>
2016-03-14 02:22:00 -0400

    Starting twitter analysis3

--- js/twitterAnalysis.js
+++ js/twitterAnalysis.js
@@ -1,13 +1,23 @@
 function twitterAnalysis(){
-
+	friends=[]
+	friends_of_friends=[]
 	cb.__call(
 		"friends_ids",
 		{},
 		function (reply,rate, err){
-			console.log(reply);
-			console.log(err);
+			friends = reply.ids;
 		}
-
-
 	);
+	for(i=0; i<friends.length;i++){
+		cb.__call(
+			"friends_ids",
+			{
+				"user_id": friends[i]
+			},
+			function (reply,rate, err){
+				friends_of_friends.append(reply.ids);
+			}
+	);
+	}
+
 }
