9070210f6d792da119c6b0dfda8dc41dfb278051
Praveen Gupta <praveen@dhcp-ccc-10160.redrover.cornell.edu>
2016-03-15 04:42:11 -0400

    Facebook comments6

--- js/instagramAnalysis.js
+++ js/instagramAnalysis.js
@@ -1,8 +1,11 @@
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
+instagram_access_token
 
+function getUserID(){
+    $.get("https://api.instagram.com/v1/users/self/?access_token="+instagram_access_token, function(response){
+        console.log(response)
+    });
+
+function instagramAnalysis(){
+    getUserID();
+    // getPhotos();
+}
