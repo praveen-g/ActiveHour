0c7af25ae1e05f94526875ba35bc7bd51a315751
Praveen Gupta <praveen@dhcp-ccc-10160.redrover.cornell.edu>
2016-03-15 04:43:18 -0400

    Instagram analysis2

--- js/instagramAnalysis.js
+++ js/instagramAnalysis.js
@@ -4,6 +4,7 @@ function getUserID(){
     $.get("https://api.instagram.com/v1/users/self/?access_token="+instagram_access_token, function(response){
         console.log(response)
     });
+}
 
 function instagramAnalysis(){
     getUserID();
