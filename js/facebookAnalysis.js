ae6a33a88d31e92feffe8d1a6b165766d01e9876
Praveen Gupta <praveen@dhcp-ccc-10160.redrover.cornell.edu>
2016-03-16 01:47:27 -0400

    Dictionaries2

--- js/facebookAnalysis.js
+++ js/facebookAnalysis.js
@@ -9,35 +9,43 @@ var commentsDict=[]
 
 var id=""
 
-function createDictionary(){
+function createCommentsDictionary(){
+	 console.log("comment")
 
-	for (var p in posts){
-		console.log(p)
-		
-		var str1 =p["message"]
-
-		if (str1!=null){
-
-			 var obj = {
-			 	"text":str1, "time": x["created_time"]
-			 }
-			 FacebookDict=FacebookDict.concat(obj)
-		}
-	}
-
-	for (var c in comments){
+	for (var c =0;c<comments.length;c++){
 		console.log(c)
 
-		var str2 =c["message"]
+		var str2 =comments[c]["message"]
 
 		if (str2!=null){
 
 			 var obj2 = {
-			 	"text":str2, "time": c["created_time"]
+			 	"text":str2, "time": comments[c]["created_time"]
 			 }
 			 FacebookDict=FacebookDict.concat(obj2)
 		} 
 	}
+}
+
+function createPostsDictionary(){
+	console.log("post")
+	for (var p=0;p<posts.length;p++){
+		console.log(p)
+		
+		var str1 =posts[p]["message"]
+
+		if (str1!=null){
+
+			 var obj = {
+			 	"text":str1, "time": posts[p]["created_time"]
+			 }
+
+			 console.log(obj)
+			 FacebookDict=FacebookDict.concat(obj)
+		}
+	}
+
+	createCommentsDictionary()
 
 }
 
