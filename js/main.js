a2ad938492273f6a70d6fa8a8373282d8f31bf94
Praveen Gupta <praveen@dhcp-ccc-10160.redrover.cornell.edu>
2016-03-16 00:32:46 -0400

    Addig amdefine2

--- js/main.js
+++ js/main.js
@@ -2,14 +2,20 @@ var mainDict=[]
 var sentimentScores = []
 var sentiment
 
-if (typeof define !== 'function') { var define = require('amdefine')(module) }
-define( function(require){
-    sentiment = require('sentiment');
-})
+// if (typeof define !== 'function') { var define = require('amdefine')(module) }
+// define( function(require){
+//     sentiment = require('sentiment');
+// })
+
 
 
 function sentimentAnalysis(socialObject){
-    var messageScore = sentiment(socialObject["text"])
+    var messageScore 
+
+    require(['foo'], function (foo) {
+        messageScore   = sentiment(socialObject["text"])
+    });
+    
     return sentimentAndTime = {
         "sentiment": messageScore, "time": socialObject["time"]
     }
