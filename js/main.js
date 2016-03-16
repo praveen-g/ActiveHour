9cddd6bfc685a9c0e6cc2ab5c57949e7ea183c81
Rishav Kanoria <rishav.kanoria@gmail.com>
2016-03-15 23:25:02 -0400

    adding sentiment analysis function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var mainDict=[]
+var sentimentScores = []
+
 function startAnalysing(){
 	var facebookMessages = facebookAnalysis();
 	var twitterMessges = twitterAnalysis();
@@ -6,4 +8,7 @@ function startAnalysing(){
     console.log(facebookMessages.length)
     console.log(twitterMessges.length)
     mainDict=facebookMessages+twitterMessges
+    for (var i = mainDict.length - 1; i >= 0; i--) {
+        sentimentScores = sentimentScores.concat(sentimentAnalysis(mainDict[i]))
+    };
 }
