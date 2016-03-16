b403f38722c70d10186df4e08ee759313d802c44
Praveen Gupta <praveen@dhcp-ccc-10160.redrover.cornell.edu>
2016-03-16 10:15:35 -0400

    trying sorting

--- js/main.js
+++ js/main.js
@@ -1,28 +1,37 @@
 var mainDict=[]
 var sentimentScores = []
 var sentiment
-
+var grouping=[]
 
 function createDict(){
-    console.log("in createDict")
+
     mainDict.forEach(function(obj,index){
+        console.log("loop")
         var messageScore = analyze(obj["text"])
         sentimentScores= sentimentScores.concat({"sentiment": messageScore, "time": obj["time"]})
+    })
+    
+    sentimentScores.forEach(function(obj,index){
+      var str=obj["time"][0]+obj["time"][1]
+      if(!grouping[str]){
+        grouping[str]=[]
+      }
+      else{
+        grouping[str]=grouping[str].concat(obj["sentiment"])
+      }
     })
 
 }
 
 function getDict(){
 
-   mainDict=twitterDict.concat(FacebookDict);
+   mainDict=(twitterDict.reverse()).concat(FacebookDict);
    createDict()
 
 }
 
 function startAnalysing(){
     console.log("in analyze")
-    var facebook= facebookAnalysis();
-    var twitter= twitterAnalysis();
-    var dict =getDict();
-
+    facebook= facebookAnalysis();
+    twitter= twitterAnalysis();
 }
