794f300967b036028ec6543aacf29f27beae30ba
Praveen Gupta <praveen@dhcp-ccc-10160.redrover.cornell.edu>
2016-03-16 10:35:51 -0400

    add 3

--- js/main.js
+++ js/main.js
@@ -4,21 +4,7 @@ var sentiment
 var grouping=[]
 
 function arrangeData(){
-  sentimentScores.forEach(function(obj,index){
-
-    var time = obj["time"][0]+ obj["time"][1]
-
-    if(grouping[time]){
-      console.log("entereing with value")
-      grouping[time]=grouping[time].concat(obj["sentiment"])
-
-    }
-    else{
-      console.log("entering wihtout value")
-      grouping[time]=[]
-    }
-  })
-  
+  console.log("display")
 }
 
 function createDict(){
@@ -27,7 +13,18 @@ function createDict(){
         console.log("loop")
         var messageScore = analyze(obj["text"])
         sentimentScores= sentimentScores.concat({"sentiment": messageScore, "time": obj["time"]})
-        
+
+        var time = obj["time"][0]+ obj["time"][1]
+
+        if(window.grouping[time]){
+          console.log("entereing with value")
+          window.grouping[time]=window.grouping[time].concat(obj["sentiment"])
+
+        }
+        else{
+          console.log("entering wihtout value")
+          window.grouping[time]=[]
+        }
     })
 
 }
